b81b775eddcb093ef0d94ecc01080623572666b5
Вадим Юлушев <centrasport@yandex.ru>
2023-02-14 19:01:55 +0300

    custom user model v1

---
+++ api_yamdb/users/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'role',
+    )
+    list_display_links = (
+        'pk',
+        'username',
+    )
+    empty_value_display = '-пусто-'
+    list_editable = ('role',)
+    search_fields = ('username',)
+    list_filter = ('role',)
+    list_select_related = False
