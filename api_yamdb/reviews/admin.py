111f1e98ef4bf1884f0c8e16e04af835b437d82d
Roman <114938872+RBychin@users.noreply.github.com>
2023-02-16 20:55:15 +0300

    false commit

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,5 +1,56 @@
 from django.contrib import admin
-from .models import Review, Comment
+
+from .models import Review, Comment, Category, Title, Genre, TitleGenres
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'year',
+        'description',
+        'category',
+    )
+    list_display_links = (
+        'name',
+    )
+    empty_value_display = '-пусто-'
+    list_editable = ('year',)
+    search_fields = ('name',)
+    list_filter = ('category',)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
+    list_display_links = (
+        'name',
+    )
+    empty_value_display = '-пусто-'
+    list_editable = ('slug',)
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'slug',
+    )
+    list_display_links = (
+        'name',
+    )
+    empty_value_display = '-none-'
+    list_editable = ('slug',)
+    search_fields = ('name',)
+
+admin.site.register(TitleGenres)
 
 
 @admin.register(Review)
