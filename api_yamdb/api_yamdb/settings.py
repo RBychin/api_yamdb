abe9cf2a6de3c1acd1341cc106cfedca1479ce2e
Лычагин Андрей <andrey.ly4agin@yandex.ru>
2023-02-20 20:17:53 +0300

    Add DEFAULT_PERMISSION_CLASSES

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -116,6 +116,9 @@ ROLE__MAX_LEN = 16
 ALLOWED_ROLES = ['moderator', 'admin']
 
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination'
                                 '.PageNumberPagination',
     'PAGE_SIZE': 5,
