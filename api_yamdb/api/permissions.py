300c45232668342fc8be9649ea8b223cd3313813
Roman <114938872+RBychin@users.noreply.github.com>
2023-02-25 23:22:47 +0300

    review update

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from users.models import User
 
 
 class IsAuthorOrReadOnly(permissions.BasePermission):
@@ -15,12 +16,12 @@ class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or (request.user.is_authenticated and (
-                    request.user.role == 'admin'
+                    request.user.role == User.ADMIN
                     or request.user.is_superuser)))
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.role == 'admin'
+                or request.user.role == User.ADMIN
                 or request.user.is_superuser)
 
 
@@ -29,7 +30,7 @@ class IsModeratorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.role == 'moderator')
+                or request.user.role == User.MODERATOR)
 
 
 class IsUserOrReadOnly(permissions.BasePermission):
@@ -37,4 +38,4 @@ class IsUserOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or request.user.role == 'user')
+                or request.user.role == User.USER)
